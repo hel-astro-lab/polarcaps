Fixes in gam_rad_comp calculation (now in code units)

init_problem.py
@@ -189,17 +189,10 @@ class Configuration_Pulsar(Configuration):
         self.gam_rad *= self.bratio**-0.5
         self.gam_rad *= ( 1.5*self.lamC/self.h_pcap/alphaf)**0.25
 
-
-        r_e = 2.8179e-13 #cm
-        deltax = 4.0*np.pi*self.cfl**2*r_e*self.Nmp
-        sigT  = 6.65246e-25  # Thomson cross section; cm^2
-        #n_x = 1e21 # cm^-3 for delgam_x = 1e-3, should calculate this directly
-        n_x = self.ninj_phots*self.xpc*deltax**-3
-        h_pcap_cm = self.h_pcap*deltax
-
+        ninj_phots_per_cell = self.ninj_phots*self.xpc
         self.gam_rad_comp = self.gam_gap**0.5
         self.gam_rad_comp *= self.delgam_x**-0.5 #m_e c^2 / kT = 1.0 / delgam_x
-        self.gam_rad_comp *= (0.28/(sigT*n_x*h_pcap_cm))**0.5
+        self.gam_rad_comp *= (0.28*6.0*np.pi*self.cfl**5*self.Nmp**2/(ninj_phots_per_cell*self.h_pcap**2))**0.5
 
         # radiation length (distance that the particle travels before reaching the limit)
         self.len_rad = (self.gam_rad/self.gam_gap)*self.h_pcap # simpler v2
@@ -267,11 +260,9 @@ class Configuration_Pulsar(Configuration):
             print("phys:        gam_rad_comp:", self.gam_rad_comp)
             print("phys:    g_rad/g_gap:", self.gam_rad/self.gam_gap)
             print("phys:    g_rad_comp/g_gap:", self.gam_rad_comp/self.gam_gap)
-            print("phys:    h_pcap_cm:", h_pcap_cm)
 
             print("phys:        len_rad:", self.len_rad)
             print("star:    xcurv:", (3.0/2.0)*self.bratio*(self.rg/self.rad_curv)*self.gam_rad**3)
-
 
         #--------------------------------------------------
         # default normalization